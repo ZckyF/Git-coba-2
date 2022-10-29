a2a9b61c1cd15f5baaa7d84f4ba41ae9a26065c7
ZckyF <zackyfirdaus407@gmail.com>
2022-10-29 20:19:09 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello Bang</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nulla accusamus tempora et eveniet quos voluptate ut saepe. Tempore nam ad, dolorum cum consectetur minus recusandae nihil eum dolores atque?</p>
+
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
